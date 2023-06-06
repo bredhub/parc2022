be2fee4d3d7ba070226f999b524a84d6ec224571
segunemma2003 <segunemma2003@gmail.com>
2023-06-06 03:33:14 +0100

    fixed something

--- src/parc_solutions/test_publisher/scripts/test1.py
+++ src/parc_solutions/test_publisher/scripts/test1.py
@@ -149,7 +149,7 @@ def estimate_distance(cv_image, focal):
     if obstacle_detected:
         min_distance = min(obstacle_distances)
         # Check if the minimum distance is below the obstacle distance threshold
-        if min_distance < 0.1:
+        if min_distance < 638:
             # Perform obstacle avoidance actions
             # Example: Stop the robot, change direction, etc.
             print("Obstacle detected. Taking avoidance action.")
